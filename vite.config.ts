dad7ce5874b3ba209a9ae494f6b4091a8ee5da20
Jess Frazelle <github@jessfraz.com>
2023-10-18 10:52:01 -0700

    Revert "add sentry"
    This reverts commit 7ccb5dc5652e82501ca13b590b126497e4f3c243.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "kittycad",
-            project: "text-to-cad-ui"
-        }
-    }), sveltekit()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
