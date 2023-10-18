1aa5acd1c6d5ed840c671c51dc5035c8e032c020
Jess Frazelle <github@jessfraz.com>
2023-10-18 14:49:05 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -7,6 +7,8 @@ export const load = async ({ cookies }) => {
 	const client = new Client(token)
 	const response = await users.get_user_self({ client })
 
+    console.log('response', response)
+
 	return {
 		user: 'error_code' in response ? undefined : response
 	}
