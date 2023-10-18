8527877682a55313b5fa091678e2c8dd2d9f91a1
Jess Frazelle <github@jessfraz.com>
2023-10-18 14:53:53 -0700

    more console logs
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,15 +1,17 @@
 import { users, Client } from '@kittycad/lib'
 
+/** @type {import('./$types').LayoutData} */
 export const load = async ({ cookies }) => {
 	let token = cookies.get('__Secure-next-auth.session-token')
 	console.log('token', token)
 
 	const client = new Client(token)
+	console.log('client', client)
 	const response = await users.get_user_self({ client })
 
-    console.log('response', response)
+	console.log('response', response)
 
 	return {
-		user: 'error_code' in response ? undefined : response
+		user: 'error_code' in response ? { user: undefined } : response
 	}
 }
