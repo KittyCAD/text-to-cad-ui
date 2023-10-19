60bc41e38225c4b786798cf1ffa5d87432168e28
Frank Noirot <frank@kittycad.io>
2023-10-18 21:52:22 -0400

    Conditionally render gLTF

--- src/components/ModelViewer.svelte
+++ src/components/ModelViewer.svelte
@@ -18,7 +18,9 @@
 </script>
 
 <SC.Canvas antialias background={new THREE.Color('oklch(99.7% 0.008766 102.8deg)')}>
-	<SC.Mesh geometry={model} />
+	{#if model}
+		<SC.Primitive object={model.scene} scale={[0.05, 0.05, 0.05]} />
+	{/if}
 	<SC.PerspectiveCamera position={[1, 1, 3]} />
 	<SC.OrbitControls enableZoom={false} />
 </SC.Canvas>
