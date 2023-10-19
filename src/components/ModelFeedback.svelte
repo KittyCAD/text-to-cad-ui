b08b3dace02cf8373c92f899f57fe9e924181824
Frank Noirot <frank@kittycad.io>
2023-10-19 16:07:07 -0400

    Add frontend UI for feedback

---
+++ src/components/ModelFeedback.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { PromptResponse } from '$lib/endpoints'
+	import ThumbsUp from './ThumbsUp.svelte'
+
+	export let modelId: string
+	export let feedback: PromptResponse['feedback']
+
+	const giveFeedback = (newFeedback: PromptResponse['feedback']) => async () => {
+		console.log(feedback, modelId, newFeedback)
+		// await fetch(`/api/feedback`, {
+		//     method: 'POST',
+		//     headers: { 'Content-Type': 'application/json' },
+		//     credentials: 'include',
+		//     body: JSON.stringify({ feedback })
+		// })
+	}
+</script>
+
+<div class="flex gap-2 justify-evenly items-center font-mono">
+	Rate
+	<button
+		on:click={giveFeedback('thumbs_down')}
+		class="hover:text-destroy-40 focus:text-destroy-40"
+	>
+		<ThumbsUp class="w-4 md:w-6 h-auto -scale-x-100 rotate-180" />
+	</button>
+	<button on:click={giveFeedback('thumbs_up')} class="hover:text-succeed-40 focus:text-succeed-40">
+		<ThumbsUp class="w-4 md:w-6 h-auto -scale-x-100" />
+	</button>
+</div>
