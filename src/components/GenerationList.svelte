406c7e2747f9c0ae7aa6e282c4f62b6e2e987061
Frank Noirot <frank@kittycad.io>
2023-10-20 16:43:30 -0400

    Turn variables into stores to keep them while navigating

--- src/components/GenerationList.svelte
+++ src/components/GenerationList.svelte
@@ -7,26 +7,26 @@
 	import type { ListLoadResponse } from '../routes/api/get-generation-list/+server'
 	import GenerationalListItemSkeleton from './GenerationalListItemSkeleton.svelte'
 	import { browser } from '$app/environment'
+	import { generations, nextPageToken } from '$lib/stores'
 
 	export let additionalGenerations: Models['TextToCad_type'][] = []
-	let generations: Models['TextToCad_type'][] = []
 
 	const filterFailures = (item: Models['TextToCad_type']) => item.status !== 'failed'
 	let PAGE_SIZE = 2
 	let isFetching = false
-	let nextPageToken: string | null = ''
 
 	async function fetchData() {
+		if ($nextPageToken === null || isFetching) return
 		isFetching = true
 
 		const response = await fetch(endpoints.localList, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			credentials: 'include',
-			body: JSON.stringify({ page_token: nextPageToken })
+			body: JSON.stringify({ page_token: $nextPageToken })
 		})
 		const nextBatchPayload = (await response.json()) as ListLoadResponse
-		nextPageToken = nextBatchPayload?.body?.next_page ?? null
+		$nextPageToken = nextBatchPayload?.body?.next_page ?? null
 		isFetching = false
 
 		updateGenerations(nextBatchPayload)
@@ -34,10 +34,12 @@
 
 	function updateGenerations(payload: ListLoadResponse) {
 		const nextBatch = payload?.body?.items ?? []
-		const newGenerations = [...generations, ...nextBatch]
-		generations = Array.from(new Set(newGenerations.map((item) => item.id))).map((id) =>
-			newGenerations.find((item) => item.id === id)
-		) as Models['TextToCad_type'][]
+		generations.update((g) => {
+			const newGenerations = [...g, ...nextBatch]
+			return Array.from(new Set(newGenerations.map((item) => item.id))).map((id) =>
+				newGenerations.find((item) => item.id === id)
+			) as Models['TextToCad_type'][]
+		})
 	}
 
 	onMount(() => {
@@ -45,7 +47,7 @@
 		fetchData()
 	})
 
-	$: console.log('generations', generations)
+	$: console.log('generations', $generations)
 </script>
 
 <section class="mt-24 mb-48">
@@ -54,14 +56,14 @@
 			>generations</span
 		>
 	</h2>
-	{#if generations.length > 0}
+	{#if $generations.length > 0}
 		<ul class="m-0 p-0">
 			{#each additionalGenerations as item}
 				<li class="first-of-type:mt-0 my-12">
 					<GenerationListItem data={item} />
 				</li>
 			{/each}
-			{#each generations.filter(filterFailures) as item}
+			{#each $generations.filter(filterFailures) as item}
 				<li class="first-of-type:mt-0 my-12">
 					<GenerationListItem data={item} />
 				</li>
@@ -75,12 +77,12 @@
 			</div>
 		{/each}
 	{/if}
-	{#if nextPageToken === null}
+	{#if $nextPageToken === null}
 		<p class="text-center text-chalkboard-70 dark:text-chalkboard-40">You're all caught up! 🎉</p>
 	{/if}
 </section>
 <InfiniteScroll
-	hasMore={nextPageToken !== null}
+	hasMore={$nextPageToken !== null}
 	threshold={200}
 	on:loadMore={fetchData}
 	elementScroll={browser ? document : undefined}
