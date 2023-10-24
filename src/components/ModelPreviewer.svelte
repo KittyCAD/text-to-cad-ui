fd61db1c840ec953961be19fb4c204e750316069
Frank Noirot <frank@kittycad.io>
2023-10-24 16:16:52 -0700

    Make camera size match the model's
    Signed-off-by: Frank Noirot <frank@kittycad.io>

--- src/components/ModelPreviewer.svelte
+++ src/components/ModelPreviewer.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import { T } from '@threlte/core'
-	import { GLTF, OrbitControls, interactivity } from '@threlte/extras'
+	import * as THREE from 'three'
+	import { T, useThrelte } from '@threlte/core'
+	import { GLTF, OrbitControls, interactivity, useGltf } from '@threlte/extras'
 	export let dataUrl: string
 	export let pausable = true
+
+	const { size: threlteSize } = useThrelte()
 
 	interactivity()
 
@@ -21,19 +24,39 @@
 			shouldAutoRotate = true
 		}, AUTO_ROTATE_PAUSE)
 	}
+
+	const loadedModel = useGltf(dataUrl)
+	let maxDistance = 0
+	$: if ($loadedModel) {
+		const size = new THREE.Vector3()
+		const boundingBox = new THREE.Box3()
+		boundingBox.setFromObject($loadedModel.scene)
+		boundingBox.getSize(size)
+		maxDistance = Math.max(size.x, size.y, size.z)
+		console.log(maxDistance)
+	}
 </script>
 
-<T.PerspectiveCamera makeDefault fov={50} position={[20, 20, 20]}>
+<T.OrthographicCamera
+	makeDefault
+	position={[maxDistance * 2, maxDistance * 2, maxDistance * 2]}
+	near={0}
+	far={maxDistance * 10}
+	left={maxDistance * -1}
+	right={maxDistance}
+	top={maxDistance * ($threlteSize.height / $threlteSize.width)}
+	bottom={maxDistance * ($threlteSize.height / $threlteSize.width) * -1}
+>
 	<OrbitControls
 		enableDamping
 		autoRotate={shouldAutoRotate}
 		on:start={disableAutoRotate}
 		on:end={reenableAutoRotate}
 	/>
-</T.PerspectiveCamera>
+</T.OrthographicCamera>
 
 <T.AmbientLight color="white" />
 <T.DirectionalLight color="white" />
 <T.DirectionalLight color="white" position={[5, 0, -5]} />
 
-<GLTF url={dataUrl} position={[0, 0, 0]} scale={300} />
+<GLTF url={dataUrl} position={[0, 0, 0]} />
