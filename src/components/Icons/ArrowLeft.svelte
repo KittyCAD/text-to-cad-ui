96e3617b3973210eae5a3f08b3fbf32f3ad8450e
Frank Noirot <frank@kittycad.io>
2023-12-22 14:43:21 -0500

    Add icon components

---
+++ src/components/Icons/ArrowLeft.svelte
@@ -0,0 +1,8 @@
+<svg {...$$restProps} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M2.29285 10L2.6464 9.64645L6.1464 6.14645L6.85351 6.85356L4.20706 9.50001L17 9.50001V10.5L4.20706 10.5L6.85351 13.1465L6.1464 13.8536L2.6464 10.3536L2.29285 10Z"
+		fill="currentColor"
+	/>
+</svg>
