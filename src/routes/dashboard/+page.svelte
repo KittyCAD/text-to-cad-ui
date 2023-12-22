65dd633354267c6200100d8658bbaa93c77a852d
Frank Noirot <frank@kittycad.io>
2023-12-22 14:46:14 -0500

    Show success message, put a cooldown timer on submission, scroll results into view + some UI polish

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -12,6 +12,9 @@
 	const getExammplePrompts = () => [...EXAMPLE_PROMPTS].sort(() => Math.random() - 0.5).slice(0, 3)
 	let examplePrompts = getExammplePrompts()
 	let error: string | null = null
+	let showSuccessMessage: boolean = false
+	let isCoolingDown = false
+	let listSection = null as HTMLDivElement | null
 
 	const submitPrompt = async (prompt: string) => {
 		const OUTPUT_FORMAT: Models['FileExportFormat_type'] = 'gltf'
@@ -41,6 +44,12 @@
 		await submitPrompt(prompt)
 		const form = e.target as HTMLFormElement
 		form.reset()
+		showSuccessMessage = true
+		isCoolingDown = true
+		setTimeout(() => {
+			isCoolingDown = false
+		}, 3000)
+		listSection?.scrollIntoView({ behavior: 'smooth', block: 'nearest' })
 	}
 
 	const retryPrompt = (e: CustomEvent<GenerationEvents['retryprompt']>) => {
@@ -50,11 +59,11 @@
 	}
 </script>
 
-<section class="mx-auto max-w-2xl my-48">
-	<h1 class="text-5xl mb-2">
+<section class="mx-auto max-w-2xl my-16 md:my-24 lg:my-48">
+	<h1 class="text-4xl md:text-5xl mb-2">
 		Text-to-<span class="text-green">CAD</span>
 	</h1>
-	<form on:submit={submitForm} class="flex w-full text-lg" bind:this={form}>
+	<form on:submit={submitForm} class="flex text-lg" bind:this={form}>
 		<label class="flex-1">
 			<span class="sr-only">Enter a text-to-CAD prompt:</span>
 			<input
@@ -64,14 +73,22 @@
 				required
 				spellcheck="false"
 				type="text"
-				class="w-full px-4 py-1 border"
+				class="w-full px-4 py-1 border border-r-0 focus:outline-none focus:bg-green/20 focus:placeholder-shown:bg-green/10"
 				bind:this={input}
+				on:input={() => {
+					showSuccessMessage = false
+					error = null
+				}}
 			/>
 		</label>
-		<button type="submit" class="submit">Submit</button>
+		<button type="submit" class="submit" disabled={isCoolingDown}>Submit</button>
 	</form>
 	{#if error}
 		<p class="text-red mt-2">{error}</p>
+	{:else if showSuccessMessage}
+		<p class="border border-t-0 border-chalkboard-70 dark:border-chalkboard-40 p-2 bg-green/40">
+			Prompt submitted!
+		</p>
 	{/if}
 	<div class="prompt-buttons">
 		<span class="font-mono pt-1 text-xs uppercase text-chalkboard-70 dark:text-chalkboard-40"
@@ -79,7 +96,6 @@
 		>
 		{#each examplePrompts as prompt (prompt)}
 			<button
-				class="submit"
 				on:click={() => {
 					if (input) {
 						input.value = prompt
@@ -92,15 +108,17 @@
 	</div>
 </section>
 {#if Boolean(data.user)}
-	<GenerationList additionalGenerations={promptedGenerations} on:retryprompt={retryPrompt} />
+	<div bind:this={listSection}>
+		<GenerationList additionalGenerations={promptedGenerations} on:retryprompt={retryPrompt} />
+	</div>
 {/if}
 
 <style lang="postcss">
 	.submit {
-		@apply px-4 lg:px-6 py-1 border border-l-0;
+		@apply px-4 lg:px-6 pt-1 pb-0.5 border;
 		@apply font-mono uppercase tracking-[1px] text-sm;
 		@apply border-chalkboard-100 dark:border-chalkboard-20;
-		@apply bg-green text-chalkboard-120 hover:hue-rotate-15;
+		@apply hover:bg-green/50;
 	}
 
 	.prompt-buttons {
@@ -108,10 +126,11 @@
 	}
 
 	.prompt-buttons button {
-		@apply text-xs rounded-full border pt-0.5 pb-0 px-3;
+		@apply text-sm rounded-full border pt-0.5 pb-0 px-3;
 		@apply bg-transparent hover:bg-green/50;
 		@apply text-chalkboard-70 dark:text-chalkboard-50 hover:text-chalkboard-120 dark:hover:text-chalkboard-10;
 		@apply border-chalkboard-30 dark:border-chalkboard-70;
 		@apply hover:border-chalkboard-120 dark:hover:border-chalkboard-10;
+		@apply hover:bg-green/20 text-chalkboard-120 hover:hue-rotate-15;
 	}
 </style>
