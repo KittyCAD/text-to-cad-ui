f647fde118186604fb1c17ce68a51929d78cc7ac
Frank Noirot <frank@kittycad.io>
2024-01-05 09:24:56 -0500

    Add better fallbacks for missing avatar image and name (#88)
    * Add better fallbacks for missing avatar image and name
    
    * Add initial fallbacks to first_name and email
    
    * Add tests for fallback behavior
    
    * update vite to latest

--- src/components/AccountMenu.svelte
+++ src/components/AccountMenu.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import type { Models } from '@kittycad/lib'
-	import Person from 'components/Icons/Person.svelte'
 	import { paths } from '$lib/paths'
+	import Person from './Icons/Person.svelte'
 
 	export let user: Models['User_type']
 	let open = false
-	let displayImage = true
+	let shouldDisplayImage = Boolean(user?.image && user.image !== '')
+	let shouldDisplayInitial =
+		!shouldDisplayImage &&
+		((user?.name && user.name.length > 0) ||
+			(user?.first_name && user.first_name.length > 0) ||
+			(user?.email && user.email.length > 0))
 
 	function dismiss(e: KeyboardEvent) {
 		if (e.key === 'Escape') {
@@ -16,32 +21,41 @@
 
 <div class={'relative flex justify-center items-center ' + (open ? 'open' : '')}>
 	<button
-		class={'toggle border border-solid hover:border-green overflow-hidden bg-currentColor w-8 h-8 md:w-12 md:h-12 ' +
-			(displayImage ? 'rounded-full' : '')}
+		class={'toggle grid place-content-center border border-solid hover:border-green overflow-hidden bg-currentColor w-8 h-8 md:w-12 md:h-12 ' +
+			(shouldDisplayInitial || shouldDisplayImage ? 'rounded-full' : 'rounded')}
 		on:click={() => {
 			open = !open
 		}}
 		on:keydown={dismiss}
 	>
-		{#if displayImage}
-			<img
-				src={user.image}
-				alt="Avatar"
-				class="object-fill"
-				referrerpolicy="no-referrer"
-				on:error={() => {
-					displayImage = false
-				}}
+		<img
+			src={user.image}
+			alt="Avatar"
+			class="object-fill"
+			style={`display: ${shouldDisplayImage ? 'block' : 'none'}`}
+			referrerpolicy="no-referrer"
+		/>
+		{#if shouldDisplayInitial}
+			<span
+				class="w-5 h-5 font-bold text-xl leading-[1] pt-0.5 text-center text-chalkboard-10 dark:text-chalkboard-120"
+				data-testid="initial">{user.name[0] || user.first_name[0] || user.email[0]}</span
+			>
+		{:else if !shouldDisplayImage}
+			<Person
+				data-testid="person-icon"
+				class="w-full text-chalkboard-10 dark:text-chalkboard-120"
 			/>
-		{:else}
-			<Person class="object-fill p-1 block text-chalkboard-10 dark:text-chalkboard-120" />
 		{/if}
 		<span class="sr-only">Open menu</span>
 	</button>
 	<dialog class="menu">
 		<menu class="contents">
 			<div class="p-4 pb-2">
-				<p class="font-mono">{user?.name || 'Unnamed User'}</p>
+				<p class="font-mono">
+					{user?.first_name
+						? user.first_name + (user.last_name ? user.last_name : '')
+						: user?.name || 'Unnamed User'}
+				</p>
 				<p class="font-mono text-sm text-chalkboard-70 dark:text-chalkboard-40">
 					{user?.email || 'someone@somewhere.com'}
 				</p>
