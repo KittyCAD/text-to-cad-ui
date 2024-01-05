0d380d662e504c7c9876fb86fa01822b67f78d28
Frank Noirot <frank@kittycad.io>
2024-01-05 09:45:00 -0500

    Rename nav to sidebar, apply only to `/view/*`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-	import Nav from 'components/Nav.svelte'
 	import 'styles/app.css'
-	import type { LayoutData } from './$types'
-
-	export let data: LayoutData
 </script>
 
-<Nav user={data ? data.user : undefined} />
 <main class="mx-2 md:mx-5 lg:mx-auto max-w-5xl">
 	<slot />
 </main>
