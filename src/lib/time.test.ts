04c2200e7d1d612a0d41c1d1eab025df1b51f57b
Frank Noirot <frank@kittycad.io>
2024-01-09 16:07:30 -0500

    Fix unit tests for time buckets

--- src/lib/time.test.ts
+++ src/lib/time.test.ts
@@ -1,7 +1,7 @@
 import {
 	msSinceStartOfDay,
 	msSinceStartOfMonth,
-	msSinceStartOfWeek,
+	msSinceAWeekAgo,
 	msSinceStartOfYear,
 	sortTimeBuckets
 } from './time'
@@ -9,15 +9,15 @@ import {
 describe('time interval tests', () => {
 	const now = new Date('2024-05-01T17:00:25.950') // which is a Wednesday
 	const expected_start_of_day = new Date('2024-05-01T00:00:00.000')
-	const expected_start_of_week = new Date('2024-04-28T00:00:00.000') // which is a Sunday
+	const expected_start_of_week = new Date('2024-04-24T00:00:00.000')
 	const expected_start_of_month = new Date('2024-05-01T00:00:00.000')
 	const expected_start_of_year = new Date('2024-01-01T00:00:00.000')
 
 	it('calculates the time to the start of the day', () => {
 		expect(msSinceStartOfDay(now)).toEqual(now.getTime() - expected_start_of_day.getTime())
 	})
-	it('calculates the time to the start of the week', () => {
-		expect(msSinceStartOfWeek(now)).toEqual(now.getTime() - expected_start_of_week.getTime())
+	it('calculates the time to a week ago', () => {
+		expect(msSinceAWeekAgo(now)).toEqual(now.getTime() - expected_start_of_week.getTime())
 	})
 	it('calculates the time to the start of the month', () => {
 		expect(msSinceStartOfMonth(now)).toEqual(now.getTime() - expected_start_of_month.getTime())
@@ -30,7 +30,7 @@ describe('time interval tests', () => {
 describe('time bucketing tests', () => {
 	it('sorts by time buckets properly', () => {
 		const items = {
-			'This Week': [],
+			'Past 7 Days': [],
 			Today: [],
 			'This Month': [],
 			'2021': [],
@@ -39,7 +39,7 @@ describe('time bucketing tests', () => {
 
 		expect(Object.entries(items).sort(sortTimeBuckets)).toEqual([
 			['Today', []],
-			['This Week', []],
+			['Past 7 Days', []],
 			['This Month', []],
 			['2023', []],
 			['2021', []]
