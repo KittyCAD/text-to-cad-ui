4c9a0da56277c0219284ca5449ef1cd772f4d739
Frank Noirot <frank@kittycad.io>
2024-01-10 22:17:07 -0500

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import 'styles/app.css'
+	import { dev } from '$app/environment'
+	import { inject } from '@vercel/analytics'
+
+	inject({ mode: dev ? 'development' : 'production' })
 </script>
 
 <slot />
