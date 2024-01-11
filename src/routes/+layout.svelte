d8043507fa66eac5757114602f48fa12d05fae80
Frank Noirot <frank@kittycad.io>
2024-01-10 22:18:20 -0500

    Add vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
 	import 'styles/app.css'
 	import { dev } from '$app/environment'
 	import { inject } from '@vercel/analytics'
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit'
 
+	injectSpeedInsights()
 	inject({ mode: dev ? 'development' : 'production' })
 </script>
 
