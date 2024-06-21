26232960383520af5f02418ccc734fcab3446256
Frank Noirot <frank@kittycad.io>
2024-06-21 02:09:34 -0400

    Add a Playwright test to verify this behavior

--- tests/e2e.playwright.ts
+++ tests/e2e.playwright.ts
@@ -1,3 +1,4 @@
+import { ITEMS_PER_PAGE, PAGES_AHEAD_TO_FETCH } from '../src/lib/consts'
 import { expect, test } from '@playwright/test'
 
 test('Redirects to the dashboard from home when logged-in', async ({ page }) => {
@@ -23,4 +24,17 @@ test('Prompt input is visible and usable on mobile', async ({ page }) => {
 	// Assert that the prompt input is visible and focused
 	await expect(page.locator('textarea')).toBeInViewport()
 	await expect(page.locator('textarea')).toBeFocused()
+})
+
+test('Sidebar only loads 5 pages of results initially', async ({ page }) => {
+	// Go to the home page
+	await page.goto('https://localhost:3000')
+
+	// Assert that we are now on the dashboard
+	await page.waitForURL('**/dashboard', { waitUntil: 'networkidle' })
+
+	// Assert that only 5 pages of results are loaded initially
+	await expect(page.getByTestId('generation-list').getByRole('link')).toHaveCount(
+		PAGES_AHEAD_TO_FETCH * ITEMS_PER_PAGE
+	)
 })
