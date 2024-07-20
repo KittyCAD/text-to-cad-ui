ea0c9f4f9a70934b9e942af78b94a88a7282ff41
Frank Noirot <frank@kittycad.io>
2024-07-20 07:46:13 -0400

    Redirect to dashboard on success

--- src/routes/success/+page.server.ts
+++ src/routes/success/+page.server.ts
@@ -2,5 +2,5 @@ import { paths } from '$lib/paths'
 import { redirect } from '@sveltejs/kit'
 
 export const load = async ({ url }) => {
-	throw redirect(308, paths.HOME + (url.search || ''))
+	throw redirect(308, paths.DASHBOARD + (url.search || ''))
 }
