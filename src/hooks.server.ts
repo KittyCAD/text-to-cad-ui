77961a6d3bbd70458e0aacec491a909f1406d848
Max Ammann <max.ammann@zoo.dev>
2025-03-07 11:42:03 +0100

    block indexing based on host not vercel env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event)
 	// On custom domains Vercel does not block indexing. We need to add it manually:
 	// https://vercel.com/guides/are-vercel-preview-deployment-indexed-by-search-engines
-	if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+	if (event.url.hostname === 'app.dev.zoo.dev') {
 		response.headers.set('X-Robots-Tag', 'noindex')
 	}
 	return response
