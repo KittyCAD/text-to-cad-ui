626d0bae8b3c101106ed5b202038835f4e0361ef
Max Ammann <max.ammann@zoo.dev>
2025-03-07 11:03:07 +0100

    add comment

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,8 @@ import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event)
+    // On custom domains Vercel does not block indexing. We need to add it manually:
+    // https://vercel.com/guides/are-vercel-preview-deployment-indexed-by-search-engines
     if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
 	    response.headers.set('X-Robots-Tag', 'noindex')
     }
