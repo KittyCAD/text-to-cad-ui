1dca9f16e9fb4f8e77944750ec50c715c65bed21
Jess Frazelle <jessfraz@users.noreply.github.com>
2025-06-15 20:19:10 -0700

    no more banner (#177)
    * no more banner
    
    * Update consts.ts
    
    * Update consts.ts

--- src/routes/(sidebarLayout)/+layout.svelte
+++ src/routes/(sidebarLayout)/+layout.svelte
@@ -6,23 +6,6 @@
 </script>
 
 <div class="h-screen overflow-hidden flex flex-col" style="height: 100dvh;">
-	<!-- A banner that warns this is not the latest version of the app -->
-	<div
-		class="top-0 left-0 right-0 z-50 text-center py-1"
-		style="background: black;color:white; position:relative"
-	>
-		<span class="font-bold">This is an old version of our ML model.</span>
-		<span class="font-bold"
-			>Please <a href="https://zoo.dev/modeling-app/download" style="border-bottom: 2px solid;"
-				>download the latest alpha version</a
-			>
-			or
-			<a href="https://zoo.dev/modeling-app/download/nightly" style="border-bottom: 2px solid;"
-				>nightly version</a
-			> (if you are feeling on the wild side) of the Modeling App.</span
-		>
-		<span class="font-bold">The newer version will be updated on this site soon.</span>
-	</div>
 	<div class="pane-layout">
 		<Sidebar user={data ? data.user : undefined} className="md:w-80" />
 		<main>
