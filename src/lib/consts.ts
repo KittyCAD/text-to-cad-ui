aa4e53222713ed9f6f2530ffb4506602b7c0aec4
Nick McCleery <n.mccleery@live.co.uk>
2025-06-17 15:24:31 +0100

    I think those are all the prompts from the website...

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -10,30 +10,21 @@ export const PLAYWRIGHT_MOCKING_HEADER = 'x-playwright-mocking'
 export const MODEL_POLLING_INTERVAL = 6000
 
 export const EXAMPLE_PROMPTS = [
-	'A dodecahedron',
-	'A camshaft',
-	'A 1/2 inch gear with 21 teeth',
-	'A 3x6 lego',
-	'A 21-tooth involute helical gear',
-	'A plate with 4 holes near each corner and rounded corners',
-	'A mounting plate that is 12 inches wide and 6 inches tall. Put four holes in each corner for #10 fasteners and fillet each corner.',
-	'A brick: 8 in. long 4 in. deep 2 in. tall—with chamfered edges',
-	'Prosthetic hip',
-	'Two gears to reduce rotation speed to half',
-	'Wing spar section',
-	"A shepherd's hook bolt",
-	'Bone plate',
-	'Cold plate',
-	'PDU faceplate',
-	'I-beam, 12 in long, 4 in high, 2.66 x 0.29 in flange, 0.19 in web, 0.46 in root radius',
-	'Surgical drill guide, 150 mm handle, Ø2 & Ø3.2 mm bits, twin bit mounts with rotating grips',
-	'A bone plate for a human femur, 8 holes, 4.5 mm screws',
+	'A 320mm vented brake rotor with 5 M12 holes on 114.3mm PCD',
 	'Gallows frame, 2400x1250x450 mm, 6 brackets, angle iron',
-	'A 320mm vented brake disc with 5 M12 holes on 114.3mm PCD',
-	'Aircraft telemetry antenna, diameter 3 in, 6 CSK holes',
-	'Angled three-finger gripper end-effector, 120 mm tall',
-	'EV battery cooling plate, 400x200x10 mm, corner mounts',
-	'Design a propellant line assembly with four mounting holes and one hole in the center of the bracket for a p-clamp. The p-clamp diameter should be 1.5 inches.'
+	'A bone plate for a human femur, 8 holes, 4.5 mm screws',
+	'Wing spar section',
+	'Sash window, 500mm wide, 1000mm high, frame 30x50, 1 vertical bar, color: brown',
+	'A 12 foot long I-beam with a depth of 10.17", a flange width of 5.750", flange thickness of 0.360", web thickness of 0.240", and fillet radius of 0.3',
+	"A shepherd's hook bolt",
+	'A car wheel',
+	'A claw hammer',
+	'Surgical drill guide, 150 mm handle, Ø2 & Ø3.2 mm bits, twin bit mounts with rotating grips',
+	'Tooling nest block, 50 mm steel cube with 20 hemispherical indentations in various sizes',
+	'I-beam, 12 in long, 4 in high, 2.66 x 0.29 in flange, 0.19 in web, 0.46 in root radius',
+	'17-floor twisted tower, 30 m wide, 5 m floor height',
+	'Cold plate',
+	'Prosthetic hip'
 ] as const
 
 export const EXAMPLES_TO_SHOW = 4
