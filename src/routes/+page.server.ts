a15e61ba46d8633689dfdb2d77115b59c8bd7efa
Jace Browning <jacebrowning@gmail.com>
2025-06-27 15:20:34 -0400

    Consolidate API token environment variables (#204)

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit'
 import { env } from '$lib/env'
 
 export const load = async ({ cookies, url }) => {
-	const token = env.PROD ? cookies.get(AUTH_COOKIE_NAME) : env.VITE_TOKEN
+	const token = env.PROD ? cookies.get(AUTH_COOKIE_NAME) : env.VITE_API_TOKEN
 
 	if (token) {
 		throw redirect(302, '/dashboard' + (url.search || ''))
--- src/routes/api/convert/[output_format]/+server.ts
+++ src/routes/api/convert/[output_format]/+server.ts
@@ -10,7 +10,7 @@ export type ConvertResponse = Models['FileConversion_type'] & {
 }
 
 export const POST: RequestHandler = async ({ cookies, fetch, request, params }) => {
-	const token = env.PROD ? cookies.get(AUTH_COOKIE_NAME) : env.VITE_TOKEN
+	const token = env.PROD ? cookies.get(AUTH_COOKIE_NAME) : env.VITE_API_TOKEN
 	if (!token) throw error(401, 'You must be logged in to use this API.')
 
 	const body = await request.text()
