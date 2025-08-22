b2d004c36e0aa184f9ab63b412e94be5567ae54c
Pierre Jacquier <pierre@zoo.dev>
2025-08-22 11:51:46 -0400

    Move logic to billing.ts file

---
+++ src/lib/billing.ts
@@ -0,0 +1,135 @@
+import type { Models } from '@kittycad/lib/types'
+import { env } from '$lib/env'
+
+// Stolen from modeling-app, need to see what we do with those
+enum Tier {
+	Free = 'free',
+	Pro = 'pro',
+	Organization = 'organization',
+	Unknown = 'unknown'
+}
+
+type OrgOrError = Models['Org_type'] | number | Error
+type SubscriptionsOrError = Models['ZooProductSubscriptions_type'] | number | Error
+type TierBasedOn = {
+	orgOrError: OrgOrError
+	subscriptionsOrError: SubscriptionsOrError
+}
+
+type ExcludeErr<T> = Exclude<T, Error>
+export function isErr<T>(value: ExcludeErr<T> | Error): value is Error {
+	return value instanceof Error
+}
+
+const toTierFrom = (args: TierBasedOn): Tier => {
+	if (typeof args.orgOrError !== 'number' && !('error_code' in args.orgOrError)) {
+		return Tier.Organization
+	} else if (typeof args.subscriptionsOrError !== 'number' && !isErr(args.subscriptionsOrError)) {
+		const subscriptions: Models['ZooProductSubscriptions_type'] = args.subscriptionsOrError
+		if (subscriptions.modeling_app.name === 'pro') {
+			return Tier.Pro
+		} else {
+			return Tier.Free
+		}
+	}
+
+	return Tier.Unknown
+}
+
+/**
+ * Copied logic from https://github.com/KittyCAD/modeling-app/blob/49d40f28b703505743f90948a38ede929d4f28e0/src/machines/billingMachine.ts#L91
+ */
+export async function getBillingInfo(token: string) {
+	const billingOrError: Models['CustomerBalance_type'] | number | Error = await fetch(
+		`${env.VITE_API_BASE_URL}/user/payment/balance`,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			}
+		}
+	)
+		.then((res) => res.json())
+		.catch((e) => {
+			console.error('Error fetching balance:', e)
+		})
+
+	if (typeof billingOrError === 'number') {
+		return new Error('Received error code: ' + billingOrError)
+	}
+
+	if (isErr(billingOrError)) {
+		return billingOrError
+	}
+
+	const billing: Models['CustomerBalance_type'] = billingOrError
+
+	const subscriptionsOrError: Models['ZooProductSubscriptions_type'] | number | Error = await fetch(
+		`${env.VITE_API_BASE_URL}/user/payment/subscriptions`,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			}
+		}
+	)
+		.then((res) => res.json())
+		.catch((e) => {
+			console.error('Error fetching subscriptions:', e)
+		})
+
+	if (typeof subscriptionsOrError === 'number') {
+		return new Error('Received error code: ' + subscriptionsOrError)
+	}
+
+	if (isErr(subscriptionsOrError)) {
+		return subscriptionsOrError
+	}
+
+	const orgOrError: Models['Org_type'] | number | Error = await fetch(
+		`${env.VITE_API_BASE_URL}/org`,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			}
+		}
+	)
+		.then((res) => res.json())
+		.catch((e) => {
+			console.error('Error fetching org:', e)
+		})
+
+	const tier = toTierFrom({
+		orgOrError,
+		subscriptionsOrError
+	})
+
+	let credits =
+		Number(billing.monthly_api_credits_remaining) + Number(billing.stable_api_credits_remaining)
+	let allowance = undefined
+
+	switch (tier) {
+		case Tier.Organization:
+		case Tier.Pro:
+			credits = Infinity
+			break
+		case Tier.Free:
+			// TS too dumb Tier.Free has the same logic
+			if (typeof subscriptionsOrError !== 'number' && !isErr(subscriptionsOrError)) {
+				allowance = Number(subscriptionsOrError.modeling_app.monthly_pay_as_you_go_api_credits)
+			}
+			break
+		case Tier.Unknown:
+			break
+	}
+
+	return {
+		tier,
+		credits,
+		allowance
+	}
+}
--- src/routes/(sidebarLayout)/+layout.server.ts
+++ src/routes/(sidebarLayout)/+layout.server.ts
@@ -4,42 +4,7 @@ import { hooksUserMocks, isUserMock } from '$lib/mocks.js'
 import { SIGN_OUT_PARAM } from '$lib/paths.js'
 import { redirect } from '@sveltejs/kit'
 import { env } from '$lib/env'
-import type { Models } from '@kittycad/lib/types'
-
-// Stolen from modeling-app, need to see what we do with those
-enum Tier {
-	Free = 'free',
-	Pro = 'pro',
-	Organization = 'organization',
-	Unknown = 'unknown'
-}
-
-type OrgOrError = Models['Org_type'] | number | Error
-type SubscriptionsOrError = Models['ZooProductSubscriptions_type'] | number | Error
-type TierBasedOn = {
-	orgOrError: OrgOrError
-	subscriptionsOrError: SubscriptionsOrError
-}
-
-type ExcludeErr<T> = Exclude<T, Error>
-function isErr<T>(value: ExcludeErr<T> | Error): value is Error {
-	return value instanceof Error
-}
-
-const toTierFrom = (args: TierBasedOn): Tier => {
-	if (typeof args.orgOrError !== 'number' && !('error_code' in args.orgOrError)) {
-		return Tier.Organization
-	} else if (typeof args.subscriptionsOrError !== 'number' && !isErr(args.subscriptionsOrError)) {
-		const subscriptions: Models['ZooProductSubscriptions_type'] = args.subscriptionsOrError
-		if (subscriptions.modeling_app.name === 'pro') {
-			return Tier.Pro
-		} else {
-			return Tier.Free
-		}
-	}
-
-	return Tier.Unknown
-}
+import { getBillingInfo, isErr } from '$lib/billing'
 
 export const load = async ({ cookies, request, url, fetch }) => {
 	if (url.searchParams.get(SIGN_OUT_PARAM)) {
@@ -82,100 +47,21 @@ export const load = async ({ cookies, request, url, fetch }) => {
 			}
 		}
 
-		const billingOrError: Models['CustomerBalance_type'] | number | Error = await fetch(
-			`${env.VITE_API_BASE_URL}/user/payment/balance`,
-			{
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${token}`
-				}
+		const billing = await getBillingInfo(token)
+		if (isErr(billing)) {
+			console.error('Error fetching billing info:', billing)
+			return {
+				user: currentUser,
+				token: token
 			}
-		)
-			.then((res) => res.json())
-			.catch((e) => {
-				console.error('Error fetching balance:', e)
-			})
-
-		if (typeof billingOrError === 'number' || isErr(billingOrError)) {
-			return Promise.reject(billingOrError)
-		}
-		console.log('billing', billingOrError)
-		const billing: Models['CustomerBalance_type'] = billingOrError
-
-		const subscriptionsOrError: Models['ZooProductSubscriptions_type'] | number | Error =
-			await fetch(`${env.VITE_API_BASE_URL}/user/payment/subscriptions`, {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${token}`
-				}
-			})
-				.then((res) => res.json())
-				.catch((e) => {
-					console.error('Error fetching subscriptions:', e)
-				})
-
-		if (typeof subscriptionsOrError === 'number' || isErr(subscriptionsOrError)) {
-			return Promise.reject(subscriptionsOrError)
 		}
 
-		const orgOrError: Models['Org_type'] | number | Error = await fetch(
-			`${env.VITE_API_BASE_URL}/org`,
-			{
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${token}`
-				}
-			}
-		)
-			.then((res) => res.json())
-			.catch((e) => {
-				console.error('Error fetching org:', e)
-			})
-
-		console.log('orgOrError', orgOrError)
-
-		const tier = toTierFrom({
-			orgOrError,
-			subscriptionsOrError
-		})
-
-		let credits =
-			Number(billing.monthly_api_credits_remaining) + Number(billing.stable_api_credits_remaining)
-		let allowance = undefined
-
-		switch (tier) {
-			case Tier.Organization:
-			case Tier.Pro:
-				credits = Infinity
-				break
-			case Tier.Free:
-				// TS too dumb Tier.Free has the same logic
-				if (typeof subscriptionsOrError !== 'number' && !isErr(subscriptionsOrError)) {
-					allowance = Number(subscriptionsOrError.modeling_app.monthly_pay_as_you_go_api_credits)
-				}
-				break
-			case Tier.Unknown:
-				break
-			default:
-				const _exh: never = tier
-		}
-
-		console.log('billing data', {
-			tier,
-			credits,
-			allowance
-		})
-
-		// Return the user and token
 		return {
 			user: currentUser,
 			token: token,
-			tier,
-			credits,
-			allowance
+			tier: billing.tier,
+			credits: billing.credits,
+			allowance: billing.allowance
 		}
 	}
 
