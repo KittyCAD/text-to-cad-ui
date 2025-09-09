ab931822984c82e06b14fcb7836fe2511ce55073
Max Ammann <max@maxammann.org>
2025-09-09 16:02:40 +0200

    adapt to new cookie names (#238)
    * adapt to new cookie names
    
    * fix pw

--- src/routes/api/submit-feedback/+server.ts
+++ src/routes/api/submit-feedback/+server.ts
@@ -1,4 +1,4 @@
-import { AUTH_COOKIE_NAME } from '$lib/cookies'
+import { getCookieName } from '$lib/cookies'
 import { endpoints } from '$lib/endpoints'
 import { error, json, type RequestHandler } from '@sveltejs/kit'
 import { env } from '$lib/env'
@@ -8,7 +8,7 @@ export type LoadResponse = {
 }
 
 export const POST: RequestHandler = async ({ cookies, fetch, request }) => {
-	const token = env.PROD ? cookies.get(AUTH_COOKIE_NAME) : env.VITE_API_TOKEN
+	const token = env.PROD ? cookies.get(getCookieName()) : env.VITE_API_TOKEN
 	const body = await request.json()
 
 	if (!(body?.id && body?.feedback))
