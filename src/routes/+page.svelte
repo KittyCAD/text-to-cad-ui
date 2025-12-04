95aab67c834dada00f2b805c0754a84c0c00a575
Persis Randolph <persis@zoo.dev>
2025-12-04 14:15:34 -0800

    format with prettier to appease lint gods

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,33 @@
-<main
-  class="mx-2 md:mx-5 lg:mx-auto min-h-screen flex items-center"
-  style="min-height: 100dvh"
->
-  <section class="mx-auto max-w-5xl flex-1">
-    <div class="flex flex-col h-full justify-between md:flex-row items-center md:items-center min-h-[33vh] gap-6 px-2 md:px-4">
+<main class="mx-2 md:mx-5 lg:mx-auto min-h-screen flex items-center" style="min-height: 100dvh">
+	<section class="mx-auto max-w-5xl flex-1">
+		<div
+			class="flex flex-col h-full justify-between md:flex-row items-center md:items-center min-h-[33vh] gap-6 px-2 md:px-4"
+		>
+			<!-- LEFT COLUMN -->
+			<div class="flex flex-col flex-1 gap-6">
+				<h1 class="font-display text-5xl md:text-6xl">
+					<span class="text-green">Text-to-CAD</span> is now available in
+					<span class="text-blue">Zoo Design Studio</span>
+				</h1>
 
-      <!-- LEFT COLUMN -->
-      <div class="flex flex-col flex-1 gap-6">
-        <h1
-          class="font-display text-5xl md:text-6xl"
-        >
-          <span class="text-green">Text-to-CAD</span> is now available in
-          <span class="text-blue">Zoo Design Studio</span>
-        </h1>
+				<a
+					href="https://app.zoo.dev/?cmd=set-layout&groupId=application&layoutId=ttc"
+					class="uppercase w-full flex items-center justify-center text-center text-white bg-blue hover:bg-blue/80 px-4 py-3 font-mono"
+				>
+					Try Zoo Design Studio in Browser
+				</a>
+			</div>
 
-        <a
-          href="https://app.zoo.dev/?cmd=set-layout&groupId=application&layoutId=ttc"
-          class="uppercase w-full flex items-center justify-center text-center text-white bg-blue hover:bg-blue/80 px-4 py-3 font-mono"
-        >
-          Try Zoo Design Studio in Browser
-        </a>
-      </div>
-
-      <!-- RIGHT COLUMN -->
-      <div class="flex-1 flex justify-center md:justify-end">
-        <video
-          src="/ttc-generate-with-ttc.mp4"
-          autoplay
-          muted
-          loop
-          class="w-full max-w-md rounded-lg"
-        ></video>
-      </div>
-
-    </div>
-  </section>
+			<!-- RIGHT COLUMN -->
+			<div class="flex-1 flex justify-center md:justify-end">
+				<video
+					src="/ttc-generate-with-ttc.mp4"
+					autoplay
+					muted
+					loop
+					class="w-full max-w-md rounded-lg"
+				/>
+			</div>
+		</div>
+	</section>
 </main>
