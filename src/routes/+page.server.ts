fda7d9e74ca805fbcf78e645e1c3e79f818ba340
Persis Randolph <persis@zoo.dev>
2025-12-04 14:46:40 -0800

    format

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,1 @@
-// import { getCookieName } from '$lib/cookies.js'
-// import { redirect } from '@sveltejs/kit'
-// import { env } from '$lib/env'
-
-export const load = async ({ cookies, url }) => {
-	// const token = env.PROD ? cookies.get(getCookieName()) : env.VITE_ZOO_API_TOKEN
-
-	// if (token) {
-	// 	throw redirect(302, '/dashboard' + (url.search || ''))
-	// }
-}
+export const load = async ({ cookies, url }) => {}
--- src/routes/success/+page.server.ts
+++ src/routes/success/+page.server.ts
@@ -1,6 +1,1 @@
-// import { paths } from '$lib/paths'
-// import { redirect } from '@sveltejs/kit'
-
-export const load = async ({ url }) => {
-	// throw redirect(308, paths.DASHBOARD + (url.search || ''))
-}
+export const load = async ({ url }) => {}
